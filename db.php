c9b8dfe9b1be6762d9392b47ab475804b17177d9
Stefanos Papadopoulos <152976197+fifers-coupe@users.noreply.github.com>
2023-12-05 20:11:03 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+class DB {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function query($sql) {
+        $result = $this->conn->query($sql);
+        return $result;
+    }
+
+    // Add other database-related functions as needed
+}
+?>
