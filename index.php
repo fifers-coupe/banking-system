6798667be05059cdbda046b347b1d5c6827bac49
Stefanos Papadopoulos <152976197+fifers-coupe@users.noreply.github.com>
2023-12-05 20:10:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Include necessary files
+require_once 'config.php';
+require_once 'db.php';
+require_once 'user.php';
+require_once 'account.php';
+
+// Your application logic goes here
+// For example, handle user authentication, display account details, etc.
+
+?>
