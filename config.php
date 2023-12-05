219344deaa7d77811dbf11d49b86016c94bfa686
Stefanos Papadopoulos <152976197+fifers-coupe@users.noreply.github.com>
2023-12-05 20:10:51 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'bank_database');
+
+// Other configuration settings
+?>
